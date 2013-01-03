faa82f2008029a85b92b7678d76d1bace990f18a
Scott Wegner <swegner2@live.com>
2013-01-02 21:41:39 -0800

    Fix various integration issues

--- Entities/CoffeeShop.cs
+++ Entities/CoffeeShop.cs
@@ -2,19 +2,11 @@
 
 namespace Entities
 {
-    [DataContract]
     public class CoffeeShop
     {
-        [DataMember]
         public int Id { get; set; }
-
-        [DataMember]
         public string YelpId { get; set; }
-
-        [DataMember]
         public string Name { get; set; }
-
-        [DataMember]
         public Coordinates Location { get; set; }
     }
 }
