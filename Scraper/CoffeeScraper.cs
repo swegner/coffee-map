92a4a068214a2a3abd035e57e930fd82ef0d452a
Scott Wegner <swegner2@live.com>
2013-01-03 06:56:40 -0800

    Modify yelp logic

--- Scraper/CoffeeScraper.cs
+++ Scraper/CoffeeScraper.cs
@@ -6,11 +6,65 @@ using YelpSharp.Data;
 using YelpSharp.Data.Options;
 using Scraper.Configuration;
 using System.Configuration;
+using System;
 
 namespace Scraper
 {
     public class CoffeeScraper
     {
+        private const double MinLatitude = 47.45;
+        private const double MaxLatitude = 47.78;
+
+        private const double MinLongitude = -122.45;
+        private const double MaxLongitude = -122.24;
+
+        private static string[] Neighborhoods = new[]
+        {
+            "Beacon Hill",
+            "Belltown",
+            "Bitter Lake",
+            "Broadview",
+            "Bryant",
+            "Capitol Hill",
+            "Central District",
+            "Columbia City",
+            "Downtown",
+            "Eastlake",
+            "First Hill",
+            "Fremont",
+            "Georgetown",
+            "Green Lake",
+            "Greenwood",
+            "Haller Lake",
+            "International District",
+            "Lake City",
+            "Laurelhurst",
+            "Madison Park",
+            "Madrona/Leschi",
+            "Magnolia",
+            "Maple Leaf",
+            "Matthews Beach",
+            "Meadowbrook",
+            "Montlake",
+            "Mt. Baker",
+            "Northgate",
+            "Phinney Ridge",
+            "Pioneer Square",
+            "Queen Anne",
+            "Rainier Beach",
+            "Rainier Valley",
+            "Ravenna",
+            "Roosevelt",
+            "Sand Point",
+            "Seward Park",
+            "SODO",
+            "University District",
+            "Wallingford",
+            "Wedgwood/View Ridge",
+            "West Seattle",
+            "White Center",
+        };
+
         public void Run()
         {
             CoffeeEntities entities = new CoffeeEntities();
@@ -30,33 +84,56 @@ namespace Scraper
                 AccessToken = yelpConfig.Token,
                 AccessTokenSecret = yelpConfig.TokenSecret,
             });
-            Task<SearchResults> searchTask = yelp.Search(new SearchOptions
-            {
-                GeneralOptions = new GeneralOptions
-                {
-                    category_filter = "Coffee",
-                },
-                LocationOptions = new LocationOptions
-                {
-                    location = "Seattle, WA",
-                },
-            });
 
-            var results = searchTask.Result;
-            var newShops = results.businesses
-                .Select(b => new CoffeeShop
-                {
-                    Name = b.name,
-                    Location = new Coordinates
-                    {
-                        Latitude = b.location.coordinate.latitude,
-                        Longitude = b.location.coordinate.longitude,
-                    },
-                    YelpId = b.id,
-                });
-            foreach (var shop in newShops)
+            foreach (string neighborhood in Neighborhoods)
             {
-                coffeeShops.Add(shop);
+                string location = string.Format("{0}, Seattle, WA", neighborhood);
+
+                int offset = 0;
+                int numFound;
+                do
+                {
+                    SearchOptions query = new SearchOptions
+                    {
+                        GeneralOptions = new GeneralOptions
+                        {
+                            category_filter = "coffee",
+                            limit = 20,
+                            offset = offset,
+                            sort = 1,
+                        },
+                        LocationOptions = new LocationOptions
+                        {
+                            location = location,
+                        },
+                    };
+                    Task<SearchResults> searchTask = yelp.Search(query);
+
+                    var results = searchTask.Result;
+                    numFound = results.businesses.Count;
+                    if (results.error != null)
+                    {
+                        throw new Exception(results.error.text);
+                    }
+
+                    var newShops = results.businesses
+                        .Select(b => new CoffeeShop
+                        {
+                            Name = b.name,
+                            Location = new Coordinates
+                            {
+                                Latitude = b.location.coordinate.latitude,
+                                Longitude = b.location.coordinate.longitude,
+                            },
+                            YelpId = b.id,
+                        });
+                    foreach (var shop in newShops)
+                    {
+                        coffeeShops.Add(shop);
+                    }
+
+                    offset += numFound;
+                } while (numFound > 0);
             }
 
             entities.SaveChanges();
