16a39b97b6c20bb35a86362735fec0c759126232
Scott Wegner <swegner2@live.com>
2013-01-03 17:39:40 -0800

    Modify scraper to sweep over grid

--- Scraper/CoffeeScraper.cs
+++ Scraper/CoffeeScraper.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Entities;
 using YelpSharp;
@@ -12,58 +14,15 @@ namespace Scraper
 {
     public class CoffeeScraper
     {
-        private const double MinLatitude = 47.45;
-        private const double MaxLatitude = 47.78;
+        private const decimal MinLatitude = 47.45M;
+        private const decimal MaxLatitude = 47.78M;
 
-        private const double MinLongitude = -122.45;
-        private const double MaxLongitude = -122.24;
+        private const decimal MinLongitude = -122.45M;
+        private const decimal MaxLongitude = -122.24M;
 
-        private static string[] Neighborhoods = new[]
-        {
-            "Beacon Hill",
-            "Belltown",
-            "Bitter Lake",
-            "Broadview",
-            "Bryant",
-            "Capitol Hill",
-            "Central District",
-            "Columbia City",
-            "Downtown",
-            "Eastlake",
-            "First Hill",
-            "Fremont",
-            "Georgetown",
-            "Green Lake",
-            "Greenwood",
-            "Haller Lake",
-            "International District",
-            "Lake City",
-            "Laurelhurst",
-            "Madison Park",
-            "Madrona/Leschi",
-            "Magnolia",
-            "Maple Leaf",
-            "Matthews Beach",
-            "Meadowbrook",
-            "Montlake",
-            "Mt. Baker",
-            "Northgate",
-            "Phinney Ridge",
-            "Pioneer Square",
-            "Queen Anne",
-            "Rainier Beach",
-            "Rainier Valley",
-            "Ravenna",
-            "Roosevelt",
-            "Sand Point",
-            "Seward Park",
-            "SODO",
-            "University District",
-            "Wallingford",
-            "Wedgwood/View Ridge",
-            "West Seattle",
-            "White Center",
-        };
+        private const decimal CoordinateStep = 0.001M;
+
+        private const double SearchOverlapPercent = 0.1;
 
         public void Run()
         {
@@ -85,58 +44,107 @@ namespace Scraper
                 AccessTokenSecret = yelpConfig.TokenSecret,
             });
 
-            foreach (string neighborhood in Neighborhoods)
+            Dictionary<string, CoffeeShop> shopDict = new Dictionary<string, CoffeeShop>();
+
+            for (decimal latitude = MinLatitude; latitude < MaxLatitude; latitude += CoordinateStep)
+            for (decimal longitude = MinLongitude; longitude < MaxLongitude; longitude += CoordinateStep)
             {
-                string location = string.Format("{0}, Seattle, WA", neighborhood);
-
-                int offset = 0;
-                int numFound;
-                do
+                SearchArea searchArea = new SearchArea
                 {
-                    SearchOptions query = new SearchOptions
+                    SouthwestCorner = new Coordinate
                     {
-                        GeneralOptions = new GeneralOptions
-                        {
-                            category_filter = "coffee",
-                            limit = 20,
-                            offset = offset,
-                            sort = 1,
-                        },
-                        LocationOptions = new LocationOptions
-                        {
-                            location = location,
-                        },
-                    };
-                    Task<SearchResults> searchTask = yelp.Search(query);
-
-                    var results = searchTask.Result;
-                    numFound = results.businesses.Count;
-                    if (results.error != null)
+                        latitude = (double)latitude + ((double)CoordinateStep * (1 + SearchOverlapPercent)),
+                        longitude = (double)longitude,
+                    },
+                    NortheastCorner = new Coordinate
                     {
-                        throw new Exception(results.error.text);
+                        latitude = (double)latitude,
+                        longitude = (double)longitude + ((double)CoordinateStep * (1 + SearchOverlapPercent)),
                     }
+                };
 
-                    var newShops = results.businesses
-                        .Select(b => new CoffeeShop
-                        {
-                            Name = b.name,
-                            Location = new Coordinates
-                            {
-                                Latitude = b.location.coordinate.latitude,
-                                Longitude = b.location.coordinate.longitude,
-                            },
-                            YelpId = b.id,
-                        });
-                    foreach (var shop in newShops)
+                IEnumerable<CoffeeShop> shops = this.SearchYelp(yelp, searchArea);
+                int numShops = 0;
+                int numNewShops = 0;
+                foreach (var coffeeShop in shops)
+                {
+                    if (!shopDict.ContainsKey(coffeeShop.YelpId))
                     {
-                        coffeeShops.Add(shop);
+                        shopDict[coffeeShop.YelpId] = coffeeShop;
+                        coffeeShops.Add(coffeeShop);
+                        numNewShops++;
                     }
+                    numShops++;
+                }
 
-                    offset += numFound;
-                } while (numFound > 0);
+                Trace.WriteLine(string.Format("SearchArea: {0} shops, {1} new", numShops, numNewShops));
             }
 
             entities.SaveChanges();
+        }
+
+        private IEnumerable<CoffeeShop> SearchYelp(Yelp yelp, SearchArea searchArea)
+        {
+            List<CoffeeShop> coffeeShops = new List<CoffeeShop>();
+
+            int offset = 0;
+            int numFound;
+            do
+            {
+                SearchOptions query = new SearchOptions
+                {
+                    GeneralOptions = new GeneralOptions
+                    {
+                        category_filter = "coffee",
+                        limit = 20,
+                        offset = offset,
+                        sort = 1,
+                    },
+                    LocationOptions = new BoundOptions
+                    {
+                        sw_latitude = searchArea.SouthwestCorner.latitude,
+                        sw_longitude = searchArea.SouthwestCorner.longitude,
+                        ne_latitude = searchArea.NortheastCorner.latitude,
+                        ne_longitude = searchArea.NortheastCorner.longitude,
+                    },
+                };
+                Task<SearchResults> searchTask = yelp.Search(query);
+
+                SearchResults results = searchTask.Result;
+                numFound = results.businesses.Count;
+                if (results.error != null)
+                {
+                    throw new Exception(results.error.text);
+                }
+
+                IEnumerable<CoffeeShop> newShops = results.businesses
+                    .Where(b => b.location.city.IndexOf("seattle", StringComparison.OrdinalIgnoreCase) != -1)
+                    .Select(b => new CoffeeShop
+                    {
+                        Name = b.name,
+                        Location = new Coordinates
+                        {
+                            Latitude = b.location.coordinate.latitude,
+                            Longitude = b.location.coordinate.longitude,
+                        },
+                        YelpId = b.id,
+                    });
+
+                foreach (var shop in newShops)
+                {
+                    coffeeShops.Add(shop);
+                }
+
+                offset += numFound;
+            } while (numFound > 0);
+
+            return coffeeShops;
+        }
+
+        private class SearchArea
+        {
+            public Coordinate SouthwestCorner { get; set; }
+            public Coordinate NortheastCorner { get; set; }
         }
     }
 }
